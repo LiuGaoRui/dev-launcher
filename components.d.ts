75807409ee7390d214fc1c9f385b9d49b69cf1ba
liugr <lgr1450807110@163.com>
2026-06-30 11:50:23 +0800

    -

--- components.d.ts
+++ components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     ActionBar: typeof import('./src/components/project/ActionBar.vue')['default']
     AppShell: typeof import('./src/components/layout/AppShell.vue')['default']
-    BuildDialog: typeof import('./src/components/project/BuildDialog.vue')['default']
     MetricsBar: typeof import('./src/components/project/MetricsBar.vue')['default']
     PlaceholderPage: typeof import('./src/components/PlaceholderPage.vue')['default']
     ProjectCard: typeof import('./src/components/project/ProjectCard.vue')['default']
